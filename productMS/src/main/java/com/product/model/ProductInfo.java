c3343aa23c78d159863be5db39c6b7c28c5e685c
ABHAY SINGH <akaskus015@gmail.com>
2025-09-08 12:00:19 +0530

    Adding new Class.

---
+++ productMS/src/main/java/com/product/model/ProductInfo.java
@@ -0,0 +1,12 @@
+package com.product.model;
+
+import lombok.Data;
+
+@Data
+public class ProductInfo {
+	private int pId;
+	private String pName;
+	private String pDesc;
+	private double pPrice;
+
+}
