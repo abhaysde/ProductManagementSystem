ca165333709ab59f460078b34bcd4b2c079bffc7
ABHAY SINGH <akaskus015@gmail.com>
2025-09-08 11:56:59 +0530

    initil commit

---
+++ productMS/src/main/java/com/product/model/TestingModel.java
@@ -0,0 +1,12 @@
+package com.product.model;
+
+import lombok.Data;
+
+@Data
+public class TestingModel {
+
+	private int id;
+	private String name;
+	private String dept;
+	private double salary;
+}
